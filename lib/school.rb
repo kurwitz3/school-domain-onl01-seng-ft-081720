8ce7da0465205e020cd8500314cdd7111f8b7c85
Richard Kurwitz <kurwitz3@aol.com>
2020-08-25 20:54:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+class School 
+
+ def initialize(name)
+   @name = name
+   @roster = {}
+ end
+  def name 
+    @name 
+  end 
+ 
+ 
+ def roster 
+   @roster 
+ end
+
+ 
+ def add_student(name,grade)
+  if @roster.include?(grade) == false
+   @roster[grade] = []
+    @roster[grade] << name
+  else 
+      @roster[grade] << name
+     end
+   end
+  def grade(num)
+    @roster.each do |key,value|
+      if num == key 
+         return value   
+      end
+     end 
+   end 
+   def sort
+     @roster.sort.to_h
+     end 
+  end 
+
